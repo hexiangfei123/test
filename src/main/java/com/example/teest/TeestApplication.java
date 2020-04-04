608527fe67ffe2e3b49e6e94251e3497906dd9ea
贺向飞 <1132310433@qq.com>
2020-04-04 19:11:37 +0800

    提交 创建  项目

---
+++ src/main/java/com/example/teest/TeestApplication.java
@@ -0,0 +1,13 @@
+package com.example.teest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeestApplication.class, args);
+    }
+
+}
